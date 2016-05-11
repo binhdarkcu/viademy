d435c85beb3617a7f9bbfbe089c87da2017194b5
Binh Lam <binh@bliss-interactive.net>
2016-05-12 00:09:38 +0700

    a

--- modules/category.php
+++ modules/category.php
@@ -1,3 +1,173 @@
 <section class="category">
-	<?php include('includes/sidebar.php');?>
+	<div class="container">
+			<?php include('includes/sidebar.php');?>
+			<div class="cateList">
+				<h2>Toàn bộ khóa học</h2>
+				<div class="filter">
+					<a href="#" class="active">Mới nhất</a>
+					<a href="#" class="">Nhiều người học nhất</a>
+					<a href="#" class="">Điểm đánh giá cao nhất</a>
+				</div>
+				<ul class="courseList">
+					<li class="free">
+						<a href="#">
+							<div class="imgBox">
+								<img src="images/img-1.jpg" alt="" />
+								<p>Miễn phí </p>
+								<div class="timeplay">
+									<i class="glyphicon glyphicon-play-circle"></i>
+									<span>25m 26s</span>
+								</div>
+							</div>
+							<h4>Lorem ipsum dolor sit amet dolor sit amet lorum ipsem cildiv covi</h4>
+						</a>
+						<div class="info">
+							<span class="per"><i class="glyphicon glyphicon-user"></i>299</span>
+							<span class="fold"><i class="glyphicon glyphicon-folder-open"></i>Danh mục 5</span>
+						</div>
+
+						<div class="byuser">
+							<img src="images/s-1.jpg" alt="" />
+							<div class="userinfo">
+								<span class="name">Nicholas Ramos</span>
+								<div class="rating">
+									<input type="text" class="rating rating-loading" value="3.5" data-size="xm" title="" >
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="premium">
+						<a href="#">
+							<div class="imgBox">
+								<img src="images/img-2.jpg" alt="" />
+								<p>Trả phí </p>
+								<div class="timeplay">
+									<i class="glyphicon glyphicon-play-circle"></i>
+									<span>25m 26s</span>
+								</div>
+							</div>
+							<h4>Lorem ipsum dolor sit amet dolor sit amet lorum ipsem cildiv covi</h4>
+						</a>
+						<div class="info">
+							<span class="per"><i class="glyphicon glyphicon-user"></i>299</span>
+							<span class="fold"><i class="glyphicon glyphicon-folder-open"></i>Danh mục 1</span>
+						</div>
+						<div class="byuser">
+							<img src="images/s-2.jpg" alt="" />
+							<div class="userinfo">
+								<span class="name">Marcus Schmidt</span>
+								<div class="rating">
+									<input type="text" class="rating rating-loading" value="3" data-size="xm" title="" >
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="free">
+						<a href="#">
+							<div class="imgBox">
+								<img src="images/img-3.jpg" alt="" />
+								<p>Miễn phí </p>
+								<div class="timeplay">
+									<i class="glyphicon glyphicon-play-circle"></i>
+									<span>25m 26s</span>
+								</div>
+							</div>
+							<h4>Lorem ipsum dolor sit amet dolor sit amet lorum ipsem cildiv covi</h4>
+						</a>
+						<div class="info">
+							<span class="per"><i class="glyphicon glyphicon-user"></i>299</span>
+							<span class="fold"><i class="glyphicon glyphicon-folder-open"></i>Danh mục 2</span>
+						</div>
+						<div class="byuser">
+							<img src="images/s-3.jpg" alt="" />
+							<div class="userinfo">
+								<span class="name">Birdie Walters</span>
+								<div class="rating">
+									<input type="text" class="rating rating-loading" value="3.5" data-size="xm" title="" >
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="free last">
+						<a href="#">
+							<div class="imgBox">
+								<img src="images/img-4.jpg" alt="" />
+								<p>Miễn phí </p>
+								<div class="timeplay">
+									<i class="glyphicon glyphicon-play-circle"></i>
+									<span>25m 26s</span>
+								</div>
+							</div>
+							<h4>Lorem ipsum dolor sit amet dolor sit amet lorum ipsem cildiv covi</h4>
+						</a>
+						<div class="info">
+							<span class="per"><i class="glyphicon glyphicon-user"></i>299</span>
+							<span class="fold"><i class="glyphicon glyphicon-folder-open"></i>Danh mục 2</span>
+						</div>
+						<div class="byuser">
+							<img src="images/s-4.jpg" alt="" />
+							<div class="userinfo">
+								<span class="name">Bess Ortiz</span>
+								<div class="rating">
+									<input type="text" class="rating rating-loading" value="3.5" data-size="xm" title="" >
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li class="free">
+						<a href="#">
+							<div class="imgBox">
+								<img src="images/img-1.jpg" alt="" />
+								<p>Miễn phí </p>
+								<div class="timeplay">
+									<i class="glyphicon glyphicon-play-circle"></i>
+									<span>25m 26s</span>
+								</div>
+							</div>
+							<h4>Lorem ipsum dolor sit amet dolor sit amet lorum ipsem cildiv covi</h4>
+						</a>
+						<div class="info">
+							<span class="per"><i class="glyphicon glyphicon-user"></i>299</span>
+							<span class="fold"><i class="glyphicon glyphicon-folder-open"></i>Danh mục 5</span>
+						</div>
+						<div class="byuser">
+							<img src="images/s-1.jpg" alt="" />
+							<div class="userinfo">
+								<span class="name">Nicholas Ramos</span>
+								<div class="rating">
+									<input type="text" class="rating rating-loading" value="3.5" data-size="xm" title="" >
+								</div>
+							</div>
+						</div>
+					</li>
+					<li class="premium">
+						<a href="#">
+							<div class="imgBox">
+								<img src="images/img-2.jpg" alt="" />
+								<p>Trả phí </p>
+								<div class="timeplay">
+									<i class="glyphicon glyphicon-play-circle"></i>
+									<span>25m 26s</span>
+								</div>
+							</div>
+							<h4>Lorem ipsum dolor sit amet dolor sit amet lorum ipsem cildiv covi</h4>
+						</a>
+						<div class="info">
+							<span class="per"><i class="glyphicon glyphicon-user"></i>299</span>
+							<span class="fold"><i class="glyphicon glyphicon-folder-open"></i>Danh mục 1</span>
+						</div>
+						<div class="byuser">
+							<img src="images/s-2.jpg" alt="" />
+							<div class="userinfo">
+								<span class="name">Marcus Schmidt</span>
+								<div class="rating">
+									<input type="text" class="rating rating-loading" value="3" data-size="xm" title="" >
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+	</div>
 </section>
--- modules/includes/sidebar.php
+++ modules/includes/sidebar.php
@@ -1,3 +1,42 @@
 <div class="sidebar">
+	<h3 class="title"><i class="glyphicon glyphicon-user"></i>Toàn bộ khóa học</h3>
 
+	<ul>
+		<li>
+			<a href="#">
+				<i class="glyphicon glyphicon-user"></i>
+				<span>Category 2</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i class="glyphicon glyphicon-user"></i>
+				<span>Category 3</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i class="glyphicon glyphicon-user"></i>
+				<span>Category 4</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i class="glyphicon glyphicon-user"></i>
+				<span>Category 5</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i class="glyphicon glyphicon-user"></i>
+				<span>Category 6</span>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<i class="glyphicon glyphicon-user"></i>
+				<span>Category 7</span>
+			</a>
+		</li>
+	</ul>
 </div>
