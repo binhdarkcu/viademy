32dcf66440d38c6987595f7c8993342865e2d368
Vuong <pvvuong@tma.com.vn>
2016-05-24 23:06:29 -0700

    Added hocphi page and thanhtoan page

---
+++ modules/payment.php
@@ -0,0 +1,50 @@
+<section class="payment section hasborder">
+	<div class="container">
+		<div class="row">
+			<h4>Thanh toán</h4>
+			<div class="tablecell">
+				<div class="paymentbox">
+					<div class="head">
+						<h5>&nbsp</h5>
+						<h2>100.000đ</h2>
+						<span>/ tháng</span>
+					</div>
+					<div class="upCon">
+						<input type="radio"> <span>Thanh toán một tháng</span>
+					</div>
+				</div>
+				<div class="paymentbox last">
+					<div class="head">
+						<h5>Ưu đãi</h5>
+						<h2>800.000đ</h2>
+						<span>/ năm</span>
+					</div>
+					<div class="upCon">
+						<input type="radio"> <span>Thanh toán một năm</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="paymentTable">
+	<div class="container">
+		<div class="row">
+			<div class="tbpayment">
+				<div class="tbhead">
+					<div class="col col1">Tổng cộng: </div>
+					<div class="col col2"></div>
+					<div class="col col3">800.000đ</div>
+					<div class="clear"></div>
+				</div>
+			</div>
+			<div class="tbpayment">
+				<div class="tbhead">
+					<div class="col col1">Thanh toán </div>
+				</div>
+			</div>
+			<a href="#" class="btn btnblue">Thanh toán</a>
+		</div>
+	</div>
+</section>
