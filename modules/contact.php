29d623da1b6932e95bbcb954583046e18f839478
SangDang <sang@bliss-interactive.com>
2016-05-27 21:11:28 +0700

    contact

--- modules/contact.php
+++ modules/contact.php
@@ -1,6 +1,42 @@
 <section>
-    <div class="container">
-        <!--background-->
-        <div class=""
-    </div>
+        <div class="contact">
+            <!--background-->
+            <div class="contact_bg">
+                <img src="images/bg-contact.png" alt="" />
+                <div class="text">
+                    <h3>Liên hệ với chúng tôi</h3>
+                    <p>Feel free and get touch with us</p>
+                </div>
+            </div>
+            <!--container-->
+            <div class="container">
+                <div class="contact_section">
+                    <div class="contact_section--form">
+                        <img src="images/contact_item--1.png" alt="" />
+                        <p>Tầng 6, tòa nhà CT1 Bắc Hà C14, Tố Hữu, Nam Từ Liêm, Hà Nội<br>Thứ 2 - Thứ 6: 08h30 - 17h00  l  Thứ 7: 08h30 - 12h00</p>
+                        <div class="text">
+                            <p>Hotline 1900 0000 00  l  Hotro@viademy.vn</p>
+                            <div class="icon">
+                                <a href="#" target="_blank"><img src="images/contact_item--2.png" alt="" /></a>
+                                <a href="#" target="_blank"><img src="images/contact_item--3.png" alt="" /></a>
+                                <a href="#" target="_blank"><img src="images/contact_item--4.png" alt="" /></a>
+                                <a href="#" target="_blank"><img src="images/contact_item--5.png" alt="" /></a>
+                            </div>
+                        </div>
+                        <form method="post">
+                            <h2>Contact us</h2>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!--start-->
+            <div class="contact_start">
+                <div class="contact_start--btn">
+                    <p>Viademy make study simple...</p>
+                    <a href="#" class="contact_start--btn--1">
+                        <span>Bắt đầu ngay</span>
+                    </a>
+                </div>
+            </div>
+        </div>
 </section>
